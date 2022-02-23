76ef16969e4c88984c843b1e0636f14cc9ea50dc
alihan939 <50833895+alihan939@users.noreply.github.com>
2022-02-23 19:02:20 +0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -222,7 +222,7 @@ get_users_list(){
 	fi
 		echo
 		clear
-		echo "Serýerdäki müşderler:"
+		echo "Serwerdäki ulanyjylar:"
 		tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | nl -s ') '
 			read -n1 -r -p "Ozalky menýuwa dolanmak üçin ENTER düwmä basyň..."
    ovpn_menu
